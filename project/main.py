88504efc14d71d3bf60c54fcff7a95af0c4a53ea
Emilie <emilieyzh@gmail.com>
2022-01-29 11:28:17 -0500

    breaking change

--- project/main.py
+++ project/main.py
@@ -1,7 +1,7 @@
 def main_wrapper():
     # function attributes are a thing!
     print(f"This is the start of our Python project. This function's name is {main_wrapper.__name__}")
-
+    BREAKING CHANGE
     print("This is the end of our Python project")
 
 
