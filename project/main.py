0a2ea72ee6c989d79d0470ab5b57818b74005c2e
Emilie <emilieyzh@gmail.com>
2022-01-29 11:27:13 -0500

    This is my second commit!

---
+++ project/main.py
@@ -0,0 +1,9 @@
+def main_wrapper():
+    # function attributes are a thing!
+    print(f"This is the start of our Python project. This function's name is {main_wrapper.__name__}")
+
+    print("This is the end of our Python project")
+
+
+if __name__ == "__main__":
+    main_wrapper()
